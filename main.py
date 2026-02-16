25bfa42d01c4edd4a383bd0ac8d6e384f75c96ab
maywaalaki <lakiup3@gmail.com>
2026-02-16 14:14:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def run_web():
 
 API_ID = 29169428
 API_HASH = '55742b16a85aac494c7944568b5507e5'
-BOT_TOKEN = '8504050677:AAF1JM3DBMAZLPuWcpyKmCU3yot0uPrAvTc'
+BOT_TOKEN = '8380607635:AAHR2fupHzfo96DSRmBkqMAb5hEwQ6m2feo'
 
 bot = TelegramClient('video_dl_bot', API_ID, API_HASH)
 
